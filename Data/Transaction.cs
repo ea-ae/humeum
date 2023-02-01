8e9fb728ff720535eaec6dbd740e38db1d4abaae
ae <90932525+ea-ae@users.noreply.github.com>
2023-02-01 22:39:14 +0200

    Add transaction counts

--- Data/Transaction.cs
+++ Data/Transaction.cs
@@ -20,15 +20,45 @@ public class Transaction {
             }
 
             TimeSpan timespan = (TimeSpan)(PaymentEnd - PaymentStart);
-            int count = (int)(Timescale.Code switch {
-                "HOURS" => timespan.TotalHours,
-                "DAYS" => timespan.TotalDays,
-                "WEEKS" => timespan.TotalDays / 7,
-                _ => throw new ArgumentOutOfRangeException($"Unexpected time interval code: {Timescale.Code}"),
-            });
-            count++; // first transaction is at the start date
+            int count = 0;
+            int years;
+            int months;
+            
+            switch (Timescale.Code) {
+                case "HOURS":
+                    count = (int)timespan.TotalHours;
+                    break;
+                case "DAYS":
+                    count = (int)timespan.TotalDays;
+                    break;
+                case "WEEKS":
+                    count = (int)(timespan.TotalDays / 7);
+                    break;
+                case "MONTHS":
+                    years = (((DateTime)PaymentEnd).Year - PaymentStart.Year) * 12;
+                    count += years;
+                    months = (((DateTime)PaymentEnd).Month - PaymentStart.Month);
+                    count += months;
 
-            return count;
+                    if (PaymentStart.AddYears(years).AddMonths(months) > (DateTime)PaymentEnd) {
+                        count -= 1; // payments occur on the same date every month
+                    }
+
+                    break;
+                case "YEARS":
+                    years = (((DateTime)PaymentEnd).Year - PaymentStart.Year);
+                    count += years;
+
+                    if (PaymentStart.AddYears(years) > (DateTime)PaymentEnd) {
+                        count -= 1; // didn't reach payment date on the last year
+                    }
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(Timescale.Code, $"Unexpected time interval code: {Timescale.Code}");
+            }
+
+            return count + 1; // first transaction is at the start date, so add 1
         }
     }
 }
