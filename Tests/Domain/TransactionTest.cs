412b25b2b0e52bf2eb5424f65d29812fbb8aaba6
ae <90932525+ea-ae@users.noreply.github.com>
2023-02-01 21:05:15 +0200

    Create transaction tests for one-time and day counts

--- Tests/Domain/TransactionTest.cs
+++ Tests/Domain/TransactionTest.cs
@@ -17,11 +17,34 @@ public class TransactionTest {
                 Code = "DAYS",
             },
             PerTimescale = false,
-            Start = new DateTime(2045, 5, 4, 12, 50, 40),
-            End = new DateTime(2045, 5, 4, 12, 50, 40),
+            PaymentStart = new DateTime(2045, 5, 4, 12, 50, 40),
+            PaymentEnd = new DateTime(2045, 5, 4, 12, 50, 40),
         };
 
         int expected = 1;
+
+        int actual = transaction.TotalTransactionCount;
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void TotalTransactionCount_Days_CountsCorrectly() {
+        var transaction = new Transaction {
+            Type = new TransactionType {
+                Name = "Income",
+                Code = "INCOME",
+            },
+            Timescale = new TransactionTimescale {
+                Name = "Days",
+                Code = "DAYS",
+            },
+            PerTimescale = false,
+            PaymentStart = new DateTime(2045, 5, 4, 12, 50, 40),
+            PaymentEnd = new DateTime(2045, 5, 10, 12, 50, 40),
+        };
+
+        int expected = (10 - 4) + 1; // 6
 
         int actual = transaction.TotalTransactionCount;
 
