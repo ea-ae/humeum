b870d71f30c0a3e5381c03da33b779d94700783c
ae <90932525+ea-ae@users.noreply.github.com>
2023-02-09 23:11:43 +0200

    Revert back to old AddTransactionCommand request structure

--- server/src/Application/Transactions/Commands/AddTransaction/AddTransactionCommand.cs
+++ server/src/Application/Transactions/Commands/AddTransaction/AddTransactionCommand.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Common;
+using Application.Common.Exceptions;
 using Application.Common.Interfaces;
 using Domain.TransactionAggregate;
 using Domain.TransactionAggregate.ValueObjects;
@@ -7,7 +8,7 @@ namespace Application.Transactions.Commands.AddTransaction;
 
 /// <summary>
 /// Create a new transaction for a user. Transactions can either be single-payment, in which
-/// case the optional RecurringTransactionFields are not provided; or they can be recurrent,
+/// case the optional recurring transaction fields are not provided; or they can be recurrent,
 /// meaning they'll be performed at a certain frequency up until the payment end date.
 /// The first payment is always made at the payment start date.
 /// </summary>
@@ -17,17 +18,9 @@ public record AddTransactionCommand : ICommand<int> {
     public required string Type { get; init; }
     public required DateTime PaymentStart { get; init; }
 
-    public RecurringTransactionFields? Recurring { get; init; }
-}
-
-/// <summary>
-/// Optional fields that are specified for transactions with recurrent payments
-/// within a specified time period and a frequency at which the payments are made.
-/// </summary>
-public record RecurringTransactionFields {
-    public required DateTime PaymentEnd { get; init; }
-    public required string TimeUnit { get; init; }
-    public required int TimesPerUnit { get; init; }
+    public DateTime? PaymentEnd { get; init; }
+    public string? TimeUnit { get; init; }
+    public int? TimesPerUnit { get; init; }
 }
 
 public class AddTransactionCommandHandler : ICommandHandler<AddTransactionCommand, int> {
@@ -36,17 +29,31 @@ public class AddTransactionCommandHandler : ICommandHandler<AddTransactionComman
     public AddTransactionCommandHandler(IAppDbContext context) => _context = context;
 
     public async Task<int> Handle(AddTransactionCommand request, CancellationToken token) {
+        // validation
+
+        List<object?> recurringTransactionFields = new() { // fields required for recurrent transactions
+            request.PaymentEnd,
+            request.TimeUnit,
+            request.TimesPerUnit
+        };
+        bool isRecurringTransaction = recurringTransactionFields.All(field => field is not null);
+        if (!isRecurringTransaction && recurringTransactionFields.Any(field => field is not null)) {
+            throw new ValidationException("Fields for recurrent transactions were only partially specified.");
+        }
+
+        // handling
+
         var transactionType = _context.GetEnumerationEntityByCode<TransactionType>(request.Type);
 
         Transaction transaction;
-        if (request.Recurring is not null) {
-            var timeUnit = _context.GetEnumerationEntityByCode<TimeUnit>(request.Recurring.TimeUnit);
-            var paymentPeriod = new TimePeriod(request.PaymentStart, request.Recurring.PaymentEnd);
-            var paymentFrequency = new Frequency(timeUnit, request.Recurring.TimesPerUnit);
+        if (isRecurringTransaction) {
+            var timeUnit = _context.GetEnumerationEntityByCode<TimeUnit>(request.TimeUnit!);
+            var paymentPeriod = new TimePeriod(request.PaymentStart, (DateTime)request.PaymentEnd!);
+            var paymentFrequency = new Frequency(timeUnit, (int)request.TimesPerUnit!);
             var paymentTimeline = new Timeline(paymentPeriod, paymentFrequency);
             transaction = new Transaction(request.Amount, transactionType, paymentTimeline);
         } else {
-            var timeline = new Timeline(new TimePeriod((DateTime)request.PaymentStart!));
+            var timeline = new Timeline(new TimePeriod(request.PaymentStart!));
             transaction = new Transaction(request.Amount, transactionType, timeline);
         }
 
