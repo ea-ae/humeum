7f3e0d30346195bbd88c83dde93f363e6e733e95
ae <90932525+ea-ae@users.noreply.github.com>
2023-03-02 16:05:33 +0200

    Create GetAssetQuery test

--- server/tests/Application.Test/AssetsTest.cs
+++ server/tests/Application.Test/AssetsTest.cs
@@ -1,6 +1,13 @@
 ﻿using Application.Assets.Commands;
+using Application.Assets.Queries;
+using Application.Common.Extensions;
+using Application.Common.Mappings;
 using Application.Test.Common;
 
+using AutoMapper;
+
+using Domain.AssetAggregate;
+using Domain.ProfileAggregate;
 using Domain.TransactionAggregate.ValueObjects;
 
 using Xunit;
@@ -27,6 +34,8 @@ public class AssetsTest {
         context.Profiles.Add(profile);
         await context.SaveChangesAsync();
 
+        // create asset
+
         AddAssetCommand command = new() {
             User = 1,
             Profile = profile.Id,
@@ -36,9 +45,6 @@ public class AssetsTest {
             StandardDeviation = 11.5m,
             AssetType = AssetType.RealEstate.Code
         };
-
-        // create asset
-
         var assetId = await handler.Handle(command);
         var asset = context.Assets.First(a => a.Id == assetId);
 
@@ -50,5 +56,40 @@ public class AssetsTest {
         Assert.Equal(command.ReturnRate, asset.ReturnRate);
         Assert.Equal(command.StandardDeviation, asset.StandardDeviation);
         Assert.Equal(command.AssetType, asset.Type.Code);
+    }
+
+    [Fact]
+    public async Task GetAssetQuery_OneAsset_ReturnsDto() {
+        var context = _dbContextFixture.CreateDbContext();
+        var mapperConfig = new MapperConfiguration(cfg => { cfg.AddProfile(new AppMappingProfile()); });
+        var handler = new GetAssetQueryHandler(context, mapperConfig.CreateMapper());
+
+        // create profile and asset
+
+        var profile = new Domain.ProfileAggregate.Profile(1, "Default");
+        var assetType = context.GetEnumerationEntityByCode<AssetType>(AssetType.Bond.Code);
+        var asset = new Asset("My asset", null, 5m, 5m, assetType, profile);
+        context.Profiles.Add(profile);
+        context.Assets.Add(asset);
+        await context.SaveChangesAsync();
+
+        // get asset DTO
+
+        GetAssetQuery query = new() { 
+            User = 1,
+            Profile = profile.Id,
+            Asset = asset.Id
+        };
+        AssetDto assetDto = await handler.Handle(query);
+
+        // confirm details
+
+        Assert.Equal(asset.Id, assetDto.Id);
+        Assert.Equal(asset.Name, assetDto.Name);
+        Assert.Equal(asset.Description, assetDto.Description);
+        Assert.Equal(asset.ReturnRate, assetDto.ReturnRate);
+        Assert.Equal(asset.StandardDeviation, assetDto.StandardDeviation);
+        Assert.Equal(assetType.Id, assetDto.Type.Id);
+        Assert.Equal(assetType.Name, assetDto.Type.Name);
     }
 }
