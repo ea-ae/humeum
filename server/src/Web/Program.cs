453ff91f74f37f54b8656749b60319548ac71ada
ae <90932525+ea-ae@users.noreply.github.com>
2023-03-21 01:24:18 +0200

    Clean up Program.cs

--- server/src/Web/Program.cs
+++ server/src/Web/Program.cs
@@ -7,26 +7,17 @@ public class Program {
     static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
 
-        // builder.Configuration.AddEnvironmentVariables();
-
         builder.Services.ConfigureApplicationServices();
         builder.Services.ConfigureInfrastructureServices(builder.Configuration);
         builder.Services.ConfigureWebServices();
 
-        // builder.Logging.AddConsole().AddFilter(level => level >= LogLevel.Trace);
-
         var app = builder.Build();
 
-        if (app.Environment.IsDevelopment()) {
-            //app.UseSwagger();
-            //app.UseSwaggerUI();
-        } else {
+        if (!app.Environment.IsDevelopment()) {
             app.MigrateDatabase();
             app.UseExceptionHandler("/Error");
-            //app.UseHsts();
         }
 
-        //app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseOpenApi();
         app.UseSwaggerUi3();
