d73f99477a43777c62b2182e4433f2d8e6af25e0
ae <90932525+ea-ae@users.noreply.github.com>
2023-04-02 13:56:48 +0300

    Create a result class

---
+++ server/src/Domain/Common/Interfaces/IResult.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Common.Interfaces;
+
+/// <summary>
+/// Result object that contains either a result value or a collection of exceptions.
+/// </summary>
+/// <typeparam name="T">Type of result in case of success.</typeparam>
+/// <typeparam name="E">List of exceptions in case of failure.</typeparam>
+public interface IResult<T, E> where E : Exception {
+    /// <summary>Whether the result is a success.</summary>
+    bool Success { get; }
+    /// <summary>Result value.</summary>
+    /// <exception cref="InvalidOperationException">In case of a failure.</exception>
+    T Value { get; }
+    /// <summary>List of errors.</summary>
+    /// <exception cref="InvalidOperationException">In case of a success.</exception>
+    IReadOnlyCollection<E> Errors { get; }
+
+    /// <summary>Try to unwrap the result value.</summary>
+    /// <param name="value">Variable to store value in.</param>
+    /// <param name="handleErrors">Action to handle a failure scenario.</param>
+    void TryUnwrap(ref T value, Action<IReadOnlyCollection<E>>? handleErrors);
+    /// <summary>Unwrap result value or provide a fallback.</summary>
+    /// <param name="fallback">Function that provides a fallback.</param>
+    /// <returns>Unwrapped value.</returns>
+    T UnwrapOr(Func<IReadOnlyCollection<E>, T> fallback);
+}
+
+/// <inheritdoc />
+public interface IResult<T> : IResult<T, Exception> { }
---
+++ server/src/Domain/Common/Models/Result.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Common.Interfaces;
+
+namespace Domain.Common.Models;
+
+/// <inheritdoc />
+public class Result<T, E> : IResult<T, E> where E : Exception {
+    public bool Success { get; private init; }
+
+    readonly T? _value;
+    public T Value => _value ?? throw new InvalidOperationException("Result did not succeed, cannot access value.");
+
+    readonly IReadOnlyCollection<E>? _errors;
+    public IReadOnlyCollection<E> Errors => _errors ?? throw new InvalidOperationException("Result succeeded, cannot access errors.");
+
+    public static Result<T, E> Ok(T value) {
+        return new Result<T, E>(value);
+    }
+
+    public static Result<T, E> Fail(E error) {
+        return new Result<T, E>(new List<E> { error });
+    }
+
+    public static Result<T, E> Fail(IReadOnlyCollection<E> errors) {
+        return new Result<T, E>(errors);
+    }
+
+    public void TryUnwrap(ref T value, Action<IReadOnlyCollection<E>>? handleErrors) {
+        if (Success) {
+            value = _value!;
+        } else if (handleErrors is not null) {
+            handleErrors.Invoke(Errors);
+        }
+    }
+
+    public T UnwrapOr(Func<IReadOnlyCollection<E>, T> fallback) => Success ? Value : fallback.Invoke(Errors);
+
+    Result(T value) {
+        Success = true;
+        _value = value;
+    }
+
+    Result(IReadOnlyCollection<E> errors) {
+        Success = false;
+        _errors = errors;
+    }
+}
