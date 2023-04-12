11c5df921838a8b3080e6193963e2d086a19980e
ae <90932525+ea-ae@users.noreply.github.com>
2023-04-12 16:10:17 +0300

    Add refresh token field to users

--- server/src/Infrastructure/Auth/ApplicationUser.cs
+++ server/src/Infrastructure/Auth/ApplicationUser.cs
@@ -4,4 +4,7 @@ namespace Infrastructure.Auth;
 
 public class ApplicationUser : IdentityUser<int> {
     public required string DisplayName { get; set; }
+
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiry { get; set; }
 }
