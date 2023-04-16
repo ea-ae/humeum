11ba73478797cb4f0dc53ce72ffbf7cbd1716f28
ae <90932525+ea-ae@users.noreply.github.com>
2023-04-16 15:34:58 +0300

    Fix controller result conversion issue again

--- server/src/Web/Filters/ApplicationResultFilterAttribute.cs
+++ server/src/Web/Filters/ApplicationResultFilterAttribute.cs
@@ -11,18 +11,22 @@ namespace Web.Filters;
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 public class ApplicationResultFilterAttribute : ResultFilterAttribute {
     /// <summary>
-    /// Unwrap the result before execution or transform it to an HTTP error.
+    /// Unwrap the result before execution or transform it to an HTTP error. In case the result itself contains an <see cref="IActionResult"/>,
+    /// unwrap the result and replace the response entirely.
     /// </summary>
     /// <param name="context">Filter context.</param>
     public override void OnResultExecuting(ResultExecutingContext context) {
         if (context.Result is ObjectResult objectResult && objectResult.Value is IResult<object?, IBaseException> objectValueResult) {
-            //if (objectValueResult.Success && objectResult.Value is IResult<IActionResult, IBaseException> actionResult) {
-            //    Console.WriteLine("???");
-            //}
-            // we are dealing with a result such as an Ok(Result<T>)
+            if (objectValueResult.Success && objectResult.Value is IResult<IActionResult, IBaseException> actionResult) {
+                // when a result contains an IActionResult, e.g.: result.Then(NoContent()), replace the response
+                context.Result = actionResult.Unwrap(); 
+            }
+            
             if (objectValueResult.Success) {
-                objectResult.Value = objectValueResult.Unwrap();
+                // when we are dealing with a result such as an Ok(Result<T>), unwrap its response contents
+                objectResult.Value = objectValueResult.Unwrap(); 
             } else {
+                // when we are dealing with a failed result, replace the response contents with an error response
                 context.Result = CreateErrorObject(objectValueResult);
             }
         } else if (context.Result is IResult<IActionResult, IBaseException> contextResult) {
